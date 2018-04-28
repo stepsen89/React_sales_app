f54d95ac0958533bd3f624b28b3ffaccd0676626
[stepsen89] <stephanie.senoner@gmail.com>
2018-04-28 22:40:55 +0200

    coursesales

--- src/App.js
+++ src/App.js
@@ -2,17 +2,24 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Coursesales from '../src/Coursesales';
+
 class App extends Component {
   render() {
+    const courses = [
+      { name: 'Complete React js course', price: 199 };
+      { name: 'Complete Pentesting course', price: 199 };
+      { name: 'Complete Front End dev course', price: 199 };
+      { name: 'Complete PHP course', price: 199 };
+    ]
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Course Purchase Page</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Coursesales items={courses}/>
       </div>
     );
   }
---
+++ src/Coursesales.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Coursesales extends Component {
+  render(){
+    return(
+      <div>
+        I am from Coursesales Component
+      </div>
+    );
+  }
+}
+
+export default Coursesales
